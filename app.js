f1cefdc91a04a66fff4c5b115505639447ce9fd5
Sebastian Misas <juanmisas2@gmail.com>
2019-04-05 12:48:40 -0700

    static files

--- app.js
+++ app.js
@@ -33,12 +33,13 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
 app.use(expressLayouts);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, staticFolder)));
+// app.use(express.static(path.join(__dirname, staticFolder)));
 
 const { COOKIE_EXPIRATION_MS } = process.env;
 app.use(
