a193c88531a20d307f8b6eb28fbe4a22febdb403
Sebastian Misas <juanmisas2@gmail.com>
2019-04-05 21:55:23 -0700

    comment out redis

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  require('dotenv').config({
+require('dotenv').config({
   path: `./env-files/${process.env.NODE_ENV || 'development'}.env`,
 });
 
@@ -7,25 +7,25 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-// const RedisStore = require('connect-redis')(session);
+const RedisStore = require('connect-redis')(session);
 
 const initAuthMiddleware = require('./features/login/init-auth-middleware');
 const indexRouter = require('./routes/index');
 
-// const redisStoreConfig = {
-//   host: process.env.REDIS_HOST,
-//   port: process.env.REDIS_PORT,
-// };
+const redisStoreConfig = {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+};
 
-// if (process.env.REDIS_URL) {
-//   redisStoreConfig.url = process.env.REDIS_URL; // this will use the REDIS_URL required for logging into the Redis addon provided by Heroku
-// }
-//
-// if (process.env.REDIS_PASSWORD) {
-//   redisStoreConfig.password = process.env.REDIS_PASSWORD; // this will use the REDIS_PASSWORD if required
-// }
-//
-// const redisStore = new RedisStore(redisStoreConfig);
+if (process.env.REDIS_URL) {
+  redisStoreConfig.url = process.env.REDIS_URL; // this will use the REDIS_URL required for logging into the Redis addon provided by Heroku
+}
+
+if (process.env.REDIS_PASSWORD) {
+  redisStoreConfig.password = process.env.REDIS_PASSWORD; // this will use the REDIS_PASSWORD if required
+}
+
+const redisStore = new RedisStore(redisStoreConfig);
 
 const staticFolder = process.env.NODE_ENV === 'development' ? 'public' : 'dist';
 const app = express();
@@ -41,21 +41,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, staticFolder)));
 
-// const { COOKIE_EXPIRATION_MS } = process.env;
-// app.use(
-//   session({
-//     store: redisStore,
-//     secret: 'keyboard cat',
-//     name: process.env.SESSION_COOKIE_NAME,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       secure: process.env.NODE_ENV === 'production',
-//       expires: Date.now() + parseInt(COOKIE_EXPIRATION_MS, 10),
-//       maxAge: parseInt(COOKIE_EXPIRATION_MS, 10),
-//     },
-//   })
-// );
+const { COOKIE_EXPIRATION_MS } = process.env;
+app.use(
+  session({
+    store: redisStore,
+    secret: 'keyboard cat',
+    name: process.env.SESSION_COOKIE_NAME,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      expires: Date.now() + parseInt(COOKIE_EXPIRATION_MS, 10),
+      maxAge: parseInt(COOKIE_EXPIRATION_MS, 10),
+    },
+  })
+);
 
 initAuthMiddleware(app);
 
